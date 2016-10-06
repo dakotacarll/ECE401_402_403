1d864ffd6210d980b6447709163cac1b51afe056
Dakota Carll <dakota.carll@maine.edu>
2016-10-06 19:37:34 -0400

    updated with led and two line lcd

--- lcd_test/lcd_test.ino
+++ lcd_test/lcd_test.ino
@@ -1,43 +1,18 @@
 
 /*
-  LiquidCrystal Library - display() and noDisplay()
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD and uses the
- display() and noDisplay() functions to turn on and off
- the display.
-
  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
+ * LCD RS pin to digital pin 2
+ * LCD Enable pin to digital pin 3
+ * LCD D4 pin to digital pin 9
+ * LCD D5 pin to digital pin 10
+ * LCD D6 pin to digital pin 11
+ * LCD D7 pin to digital pin 22
  * LCD R/W pin to ground
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalDisplay
-
- */
-
+*/
 // include the library code:
 #include <LiquidCrystal.h>
 
@@ -48,6 +23,8 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16,2);
  
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(7, OUTPUT);
   
 }
 
@@ -62,5 +39,13 @@ void loop()
   lcd.print("Voltage      ");  
   lcd.setCursor(12, 1);
   lcd.print("2.4V");
-  delay(500);
+  delay(500); 
+
+  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
+
+
 }
+
